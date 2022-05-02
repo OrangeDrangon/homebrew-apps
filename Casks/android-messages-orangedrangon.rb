ea0df06e3ac5455a6bb821905a11ca4e820e8f67
Kyle Rosenberg <kyle@ekrosenberg.com>
2022-05-01 22:49:41 -0400

    add android-messages-desktop

---
+++ Casks/android-messages-orangedrangon.rb
@@ -0,0 +1,24 @@
+# typed: false
+# frozen_string_literal: true
+
+cask "android-messgaes-orangedragon" do
+    version "5.3.4"
+    sha256 :no_check
+  
+    url "https://github.com/OrangeDrangon/android-messages-desktop/releases/download/v#{version}/Android-Messages-v#{version}-mac-universal.zip"
+    name "Android Messages"
+    desc "Android Messages as a Cross-platform Desktop App"
+    homepage "https://github.com/OrangeDrangon/android-messages-desktop"
+  
+    depends_on arch: :intel
+    depends_on arch: :arm64
+  
+    app "Android Messages.app"
+  
+    uninstall trash: [
+      "~/Library/Application\ Support/Android\ Messages",
+      "~/Library/Preferences/pw.kmr.amd.plist",
+      "~/Library/Saved\ Application\ State/pw.kmr.amd.savedState",
+    ]
+  end
+  
