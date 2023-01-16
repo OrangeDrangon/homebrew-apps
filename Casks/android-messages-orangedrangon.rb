12f41dae80c126abc4d52cf1b9801791836d5716
seanwo <seanwo@gmail.com>
2023-01-16 14:38:52 -0600

    version bump to 5.4.1

--- Casks/android-messages-orangedrangon.rb
+++ Casks/android-messages-orangedrangon.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 cask "android-messages-orangedrangon" do
-  version "5.4.0"
+  version "5.4.1"
   sha256 :no_check
 
   url "https://github.com/OrangeDrangon/android-messages-desktop/releases/download/v#{version}/Android-Messages-v#{version}-mac-universal.zip"
