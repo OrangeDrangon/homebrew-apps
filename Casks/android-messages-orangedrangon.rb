3ab30a9fc59adc3757d7502fc0f4f96200ef35b4
Kyle Rosenberg <kyle@ekrosenberg.com>
2024-06-08 12:10:25 -0400

    bump version

--- Casks/android-messages-orangedrangon.rb
+++ Casks/android-messages-orangedrangon.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 cask "android-messages-orangedrangon" do
-  version "5.4.2"
+  version "5.4.4"
   sha256 :no_check
 
   url "https://github.com/OrangeDrangon/android-messages-desktop/releases/download/v#{version}/Android-Messages-v#{version}-mac-universal.zip"
