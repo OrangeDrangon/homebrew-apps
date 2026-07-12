065f1005b87a3246aace0d0ff8b0bc625f0b1c76
OrangeDrangon <25403517+OrangeDrangon@users.noreply.github.com>
2026-07-12 15:41:32 -0400

    Update android-messages-orangedrangon.rb

--- Casks/android-messages-orangedrangon.rb
+++ Casks/android-messages-orangedrangon.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 cask "android-messages-orangedrangon" do
-  version "6.0.0"
+  version "6.1.0"
   sha256 :no_check
 
   url "https://github.com/OrangeDrangon/android-messages-desktop/releases/download/v#{version}/Android-Messages-v#{version}-mac-universal.zip"
